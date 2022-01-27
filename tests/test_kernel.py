7dc32f660472fbc597d111d7a915fa7a18f72ea9
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-27 15:53:49 +0100

    cleaned up

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,27 +2,34 @@
 
 
 import jax.numpy as jnp
-import pytest
+import pytest_cases
 
 from pnfindiff import kernel
 
 
-@pytest.fixture
-def exp_quad():
+def case_exp_quad():
     k = lambda x, y: jnp.exp(-(x - y).dot(x - y))
     return kernel.gram_matrix_function(k)
 
 
+def case_exp_quad_builtin():
+    k = lambda x, y: jnp.exp(-(x - y).dot(x - y))
+    return kernel.gram_matrix_function(k)
+
+
+@pytest_cases.parametrize_with_cases("exp_quad", cases=".")
 def test_gram_matrix_function_pairwise(exp_quad):
     xs = jnp.arange(4)
     assert exp_quad(xs, xs).shape == ()
 
 
+@pytest_cases.parametrize_with_cases("exp_quad", cases=".")
 def test_gram_matrix_function_diagonal(exp_quad):
     xs = jnp.arange(8).reshape((4, 2))
     assert exp_quad(xs, xs).shape == (4,)
 
 
+@pytest_cases.parametrize_with_cases("exp_quad", cases=".")
 def test_gram_matrix_function_outer(exp_quad):
     xs = jnp.arange(8).reshape((4, 2))
     ys = jnp.arange(12).reshape((6, 2))
