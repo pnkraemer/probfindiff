cbf9556e30164f4c9ee457a75b7b99a2c1a9ce06
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-27 15:49:50 +0100

    gram matrix complete

--- src/pnfindiff/kernel.py
+++ src/pnfindiff/kernel.py
@@ -1,12 +1,27 @@
 """Kernel function functionality."""
 
+import jax
+
 
 def gram_matrix_function(k):
+
+    k_inner = jax.vmap(k, (0, 0), 0)
+    k_outer = jax.vmap(jax.vmap(k, (0, None), 0), (None, 1), 1)
+
     def k_wrapped(xs, ys):
 
         # Single element of the Gram matrix:
         # X.shape=(d,), Y.shape=(d,) -> K.shape = ()
         if xs.ndim == ys.ndim <= 1:
             return k(xs, ys)
+
+        # Diagonal of the Gram matrix:
+        # X.shape=(N,d), Y.shape=(N,d) -> K.shape = (N,)
+        if xs.shape == ys.shape:
+            return k_inner(xs, ys)
+
+        # Full Gram matrix:
+        # X.shape=[N,d), Y.shape=(d,K) -> K.shape = (N,K)
+        return k_outer(xs, ys)
 
     return k_wrapped
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -16,3 +16,14 @@ def exp_quad():
 def test_gram_matrix_function_pairwise(exp_quad):
     xs = jnp.arange(4)
     assert exp_quad(xs, xs).shape == ()
+
+
+def test_gram_matrix_function_diagonal(exp_quad):
+    xs = jnp.arange(8).reshape((4, 2))
+    assert exp_quad(xs, xs).shape == (4,)
+
+
+def test_gram_matrix_function_outer(exp_quad):
+    xs = jnp.arange(8).reshape((4, 2))
+    ys = jnp.arange(12).reshape((6, 2))
+    assert exp_quad(xs, ys.T).shape == (4, 6)
