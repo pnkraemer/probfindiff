226e4e564127849985ab40290d10a25011e81f8a
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-28 14:18:04 +0100

    diffop kwargs

--- src/pnfindiff/aux/diffops.py
+++ src/pnfindiff/aux/diffops.py
@@ -4,29 +4,29 @@ import jax
 import jax.numpy as jnp
 
 
-def deriv_scalar(fun, *, argnums=0):
+def deriv_scalar(fun, /, **kwargs):
     """Derivative of a scalar function."""
 
-    grad = jax.grad(fun, argnums=argnums)
+    grad = jax.grad(fun, **kwargs)
     return lambda *args: grad(*args).squeeze()
 
 
-def div(fun, *, argnums=0):
+def div(fun, /, **kwargs):
     """Divergence of a function as the trace of the Jacobian."""
 
-    jac = jax.jacrev(fun, argnums=argnums)
+    jac = jax.jacrev(fun, **kwargs)
     return lambda *args: jnp.trace(jac(*args))
 
 
-def laplace(fun, *, argnums=0):
+def laplace(fun, /, **kwargs):
     """Laplace operator."""
-    return compose(div, jax.grad)(fun, argnums=argnums)
+    return compose(div, jax.grad)(fun, **kwargs)
 
 
 def compose(op1, op2):
     """Compose two differential operators."""
 
-    def D(fun, *, argnums=0):
-        return op1(op2(fun, argnums=argnums), argnums=argnums)
+    def D(fun, /, **kwargs):
+        return op1(op2(fun, **kwargs), **kwargs)
 
     return D
