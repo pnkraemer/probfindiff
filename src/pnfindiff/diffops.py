fc1475da29ebd6771355c53beae1e8c98e0f5826
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-28 13:02:57 +0100

    fixed pylint

--- src/pnfindiff/diffops.py
+++ src/pnfindiff/diffops.py
@@ -4,16 +4,6 @@ import jax
 import jax.numpy as jnp
 
 
-def div():
-    """Divergence of a function as the trace of the Jacobian."""
-
-    def D(fun, *, argnums=0):
-        jac = jax.jacrev(fun, argnums=argnums)
-        return lambda *args: jnp.trace(jac(*args))
-
-    return D
-
-
 def deriv_scalar():
     """Derivative of a scalar function."""
 
@@ -24,12 +14,24 @@ def deriv_scalar():
     return D
 
 
+def div():
+    """Divergence of a function as the trace of the Jacobian."""
+
+    def D(fun, *, argnums=0):
+        jac = jax.jacrev(fun, argnums=argnums)
+        return lambda *args: jnp.trace(jac(*args))
+
+    return D
+
+
 def laplace():
     """Laplace operator."""
     return compose(div(), jax.grad)
 
 
 def compose(op1, op2):
+    """Compose two differential operators."""
+
     def D(fun, *, argnums=0):
         return op1(op2(fun, argnums=argnums), argnums=argnums)
 
