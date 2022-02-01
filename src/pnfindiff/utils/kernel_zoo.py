492bdf750522d3d8bc9df3346b61bc655b4b6dfe
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-02-01 15:28:12 +0100

    kernel zoo docs

--- src/pnfindiff/utils/kernel_zoo.py
+++ src/pnfindiff/utils/kernel_zoo.py
@@ -12,7 +12,29 @@ import jax.numpy as jnp
 def exponentiated_quadratic(
     x: Any, y: Any, input_scale: float = 1.0, output_scale: float = 1.0
 ) -> Any:
-    """Exponentiated quadratic kernel."""
+    r"""Exponentiated quadratic kernel.
+
+    The kernel is defined as
+
+    .. math:: k(x,y) = \sigma \exp(-\epsilon \|x-y\|^2/2)
+
+    Parameters
+    ----------
+    x
+        Input variable.
+    y
+        Input variable.
+    input_scale
+        Input scale :math:`\epsilon` of the kernel.
+    output_scale
+        Output scale :math:`\sigma` of the kernel.
+
+
+    Returns
+    -------
+    :
+        Evaluation :math:`k(x,y)`.
+    """
     return output_scale * jnp.exp(-input_scale * (x - y).dot(x - y) / 2.0)
 
 
@@ -20,5 +42,28 @@ def exponentiated_quadratic(
 def polynomial(
     x: Any, y: Any, *, order: int = 2, scale: float = 1.0, bias: float = 1.0
 ) -> Any:
-    """Polynomial kernels."""
+    r"""Polynomial kernels.
+
+    The kernel is defined as
+
+    .. math:: k(x,y) = (a \langle x, y\rangle + b)^c
+
+    Parameters
+    ----------
+    x
+        Input variable.
+    y
+        Input variable.
+    scale
+        Scale :math:`a`.
+    bias
+        Bias :math:`b`.
+    order
+        Order :math:`c`.
+
+    Returns
+    -------
+    :
+        Evaluation :math:`k(x,y)`.
+    """
     return (scale * x.dot(y) + bias) ** order
