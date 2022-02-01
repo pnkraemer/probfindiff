7101e383959012a0394e5808039bca9fb55ffced
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-02-01 16:43:07 +0100

    derivative higher in public api

--- src/pnfindiff/_api.py
+++ src/pnfindiff/_api.py
@@ -2,9 +2,14 @@
 
 from pnfindiff import coefficients
 
-__all__ = ["derivative"]
+__all__ = ["derivative", "derivative_higher"]
 
 
 def derivative(f, **kwargs):
     coeffs, indices = coefficients.derivative(**kwargs)
+    return coefficients.apply(f, coeffs=coeffs, indices=indices)
+
+
+def derivative_higher(f, **kwargs):
+    coeffs, indices = coefficients.derivative_higher(**kwargs)
     return coefficients.apply(f, coeffs=coeffs, indices=indices)
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,12 +6,23 @@ import pnfindiff
 from pnfindiff.utils import autodiff
 
 
-@pytest.mark.parametrize("f,df_true,L", ((jnp.sin, jnp.cos, autodiff.derivative),))
-def test_derivative(f, df_true, L):
+@pytest.mark.parametrize("f,df_true", ((jnp.sin, jnp.cos),))
+def test_derivative(f, df_true):
     xs = jnp.linspace(0.0, 1.0, num=12)
 
     fx = f(xs)
     dfx_true = df_true(xs)
     dfx_approx, _ = pnfindiff.derivative(fx, xs=xs)
+
+    assert jnp.allclose(dfx_approx, dfx_true, atol=1e-3, rtol=1e-3)
+
+
+@pytest.mark.parametrize("f,df_true", ((jnp.sin, jnp.cos),))
+def test_derivative_higher(f, df_true):
+    xs = jnp.linspace(0.0, 1.0, num=12)
+
+    fx = f(xs)
+    dfx_true = df_true(xs)
+    dfx_approx, _ = pnfindiff.derivative_higher(fx, deriv=1, xs=xs)
 
     assert jnp.allclose(dfx_approx, dfx_true, atol=1e-3, rtol=1e-3)
