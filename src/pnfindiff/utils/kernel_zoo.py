8a86ed0fadc8571b93fb49dc9dd446ff75b29d51
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-02-01 13:32:03 +0100

    kernel zoo types

--- src/pnfindiff/utils/kernel_zoo.py
+++ src/pnfindiff/utils/kernel_zoo.py
@@ -2,18 +2,23 @@
 
 
 from functools import partial
+from typing import Any
 
 import jax
 import jax.numpy as jnp
 
 
 @jax.jit
-def exponentiated_quadratic(x, y, input_scale=1.0, output_scale=1.0):
+def exponentiated_quadratic(
+    x: Any, y: Any, input_scale: float = 1.0, output_scale: float = 1.0
+) -> Any:
     """Exponentiated quadratic kernel."""
     return output_scale * jnp.exp(-input_scale * (x - y).dot(x - y) / 2.0)
 
 
 @partial(jax.jit, static_argnames=("scale", "order", "bias"))
-def polynomial(x, y, *, order=2, scale=1.0, bias=1.0):
+def polynomial(
+    x: Any, y: Any, *, order: int = 2, scale: float = 1.0, bias: float = 1.0
+) -> Any:
     """Polynomial kernels."""
     return (scale * x.dot(y) + bias) ** order
