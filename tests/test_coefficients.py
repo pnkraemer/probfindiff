bccdaf14a222b9b90e3bc9910e759982db294383
Nicholas Krämer <nicokraemer@gmx.net>
2022-02-01 07:41:41 +0100

    api is coefficients now

--- tests/test_coefficients.py
+++ tests/test_coefficients.py
@@ -3,13 +3,13 @@
 
 import jax.numpy as jnp
 
-import pnfindiff
+from pnfindiff import coefficients
 
 
 def test_findiff():
     x = jnp.linspace(1, 1.1, 10)
     f = jnp.sin(x)
-    fd = pnfindiff.findiff(xs=x, deriv=1, num=3)
+    fd = coefficients.findiff(xs=x, deriv=1, num=3)
 
     df_approx, _ = fd(f)
 
@@ -22,7 +22,7 @@ def test_findiff_along_axis():
     f = jnp.sin(x)[:, None] * jnp.sin(y)[None, :]
     df_dy = jnp.sin(x)[:, None] * jnp.cos(y)[None, :]
 
-    fd = pnfindiff.findiff_along_axis(axis=1, xs=x, deriv=1, num=3)
+    fd = coefficients.findiff_along_axis(axis=1, xs=x, deriv=1, num=3)
     df_approx, _ = fd(f)
 
     assert jnp.allclose(df_approx, df_dy, atol=1e-4, rtol=1e-4)
