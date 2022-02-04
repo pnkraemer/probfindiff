1870ba906b207f9f91c634e5d4849058c3aafdff
Nicholas Krämer <nicokraemer@gmx.net>
2022-02-04 12:47:46 +0100

    failing nd.from_grid test

--- tests/test_nd.py
+++ tests/test_nd.py
@@ -13,10 +13,10 @@ def test_from_grid():
     diffop = jax.jacfwd
     xs_1d = jnp.array([1.0, 2.0, 3.0])
     xs = stencil.multivariate(xs_1d, shape_input=(2,))
-    assert xs.shape == (2, 2, 3)
+    assert xs.shape == (1, 2, 2, 3)
 
-    fx = jnp.einsum("abc,abc->ac", xs, xs)[:, None, :]
-    assert fx.shape == (2, 1, 3)
+    fx = jnp.einsum("...bc,...bc->...c", xs, xs)[:, :, None, :]
+    assert fx.shape == (1, 2, 1, 3)
 
     scheme = nd.from_grid(diffop, xs=xs)
     dfx, _ = nd.differentiate(fx, scheme=scheme)
